c1fd6e4178ab0232f347443a5c48af09803fc277
aparekh29 <aparekh29@gmail.com>
2022-06-05 20:34:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text("🥗 Kale, Spinach and Rocket Smoothie")
 streamlit.text("🐔 Hard-boiled and Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
