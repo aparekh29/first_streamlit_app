607ad6bc9b15edd696e0239ea945b470c4fb241c
aparekh29 <aparekh29@gmail.com>
2022-06-07 15:04:41 -0700

    Search my fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ streamlit.text("🥑🍞 Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv( 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt' )
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
 # Let's put a multi-select here so that user can pick the fruit he/she likes.
 streamlit.multiselect( "Pick some fruit" , list( my_fruit_list.index ) )
